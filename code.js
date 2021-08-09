da038f56c9b2a50e017b0c514eb45e78e08ab008
Gregory Reeves <gregreeves280@gmail.com>
2021-08-08 23:06:39 -0400

    updated

--- code.js
+++ code.js
@@ -2,92 +2,81 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
-let body = document.querySelector("body")
-let main = document.createElement('main')
-document.body.appendChild(main)
-// let div1 = document.createElement(`div`)
-// div1.append(body);
-// let div2 = document.createElement(`div`)
-// div2.append(body);
+let body = document.querySelector("body");
+let main = document.createElement("main");
+document.body.appendChild(main);
 
-const startButton = document.createElement("button")
-startButton.innerHTML = `Start Tracker`
+const startButton = document.createElement("button");
+startButton.innerHTML = `Start`;
 document.body.appendChild(startButton);
-// const stopButton = document.createElement("button")
-// stopButton.innerHTML = `Stop Tracker`
-// document.body.appendChild(stopButton);
 
-let start = startButton.addEventListener(`click`,() =>{
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            console.log(position)
-            requestPhotos(position.coords);
-        },
-        positionError => {
-            alert(`Could not get your position`);
-            requestPhotos(fallBackLocation);
-            console.log(positionError)
-        }
-    );
+const replayButton = document.createElement("button");
+replayButton.style.top = "80px"
+replayButton.innerHTML = `Replay`;
+document.body.appendChild(replayButton);
+
+
+let start = startButton.addEventListener(`click`, () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      requestPhotos(position.coords);
+    },
+    (positionError) => {
+      alert(`Could not get your position`);
+      requestPhotos(fallBackLocation);
+      console.log(positionError);
+    }
+  );
 });
 
-const trackUser = navigator.geolocation.watchPosition(
-    success => {
-            console.log(success)
-            requestPhotos(success.coords);
-        },
-);
+const trackUser = navigator.geolocation.watchPosition((success) => {
+  console.log(success);
+  requestPhotos(success.coords);
+});
 
 navigator.geolocation.clearWatch(trackUser);
 
-const fallBackLocation = {latitude: 28.474386, longitude: -81.468193}
+const fallBackLocation = { latitude: 28.474386, longitude: -81.468193 };
 
-function requestPhotos(location){
-    console.log(`Requesting Photos near me ${location.latitude}, ${location.longitude}`)
-    let url = (`https://shrouded-mountain-15003.herokuapp.com/https://api.flickr.com/services/rest/?api_key=0f1a62f39c3f42f6f76b15680e600792&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&${location.latitude}&${location.longitude}&text=car`)
+function requestPhotos(location) {
+  console.log(
+    `Requesting Photos near me ${location.latitude}, ${location.longitude}`
+  );
+  let url = `https://shrouded-mountain-15003.herokuapp.com/https://api.flickr.com/services/rest/?api_key=0f1a62f39c3f42f6f76b15680e600792&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&${location.latitude}&${location.longitude}&text=supercar`;
 
-    let fetchPromise = fetch(url)
-    fetchPromise.then(processResponse)
+  let fetchPromise = fetch(url);
+  fetchPromise.then(processResponse);
 }
 
-function assembleUrlSourceImage (photoObj){
-    return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}.jpg`;
+function assembleUrlSourceImage(photoObj) {
+  return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${photoObj.secret}.jpg`;
 }
 
-function processResponse (response) {
-    let responsePromise = response.json()
-    responsePromise.then(showPhotos)
+function processResponse(response) {
+  let responsePromise = response.json();
+  responsePromise.then(showPhotos);
 }
 
-var photosArray = []
-var index = 0
+var photosArray = [];
+var index = -1;
 
-function showPhotos (data){
-    console.log(data)
-    photosArray = data.photos.photo
-    console.log(photosArray)
-    
-    for(const index in photosArray){
-    let div = document.createElement('div')
-    var img = document.createElement('img');
-    div.innerHTML = assembleUrlSourceImage(photosArray[index])
-    div.append(img)
-    img.src =(assembleUrlSourceImage(photosArray[index]))
-    main.append(div)
-    }
+function showPhotos(data) {
+  console.log(data);
+  photosArray = data.photos.photo;
+  console.log(photosArray);
+  
+  if (index < photosArray.length) {
+    index++;
+    let img = document.createElement("img");
+    img.src = assembleUrlSourceImage(photosArray[index]);
+    main.innerHTML = `<a href="${assembleUrlSourceImage(photosArray[index])}">Click Here.</a>`;
+    main.append(img);
+  } else {
+      console.log("Done")
+  }
 }
 
-
-// let morePhotos = document.createElement('button')
-// morePhotos.innerHTML = "More Photos"
-// morePhotos.addEventListener('click', () =>{
-//     showPhotos()
-// });
-// main.append(morePhotos);
-// displayPhotoObject(photoObj) {
-//     let imageUrl = this.assembleUrlSourceImage(photoObj);
-//     let img = document.createElement('img')
-//     img.src = imageUrl
-//     main.innerHTML = ''
-//     main.append(img)
-// }
+let replay = replayButton.addEventListener(`click`, () => {
+location.reload()
+})
